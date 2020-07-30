4e947cb85dc9611fc26d2832c05512ba3cacbfc2
zhangyoubao <18301699319@139.com>
2020-07-30 19:41:42 +0800

    开启mysql打印日志

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -114,4 +114,15 @@ public class PassportController {
 
     }
 
+
+    @ApiOperation(value = "用户退出登录", notes = "用户退出登录", httpMethod = "POST")
+    @PostMapping("logout")
+    public IMOOCJSONResult logout(@RequestParam String userId, HttpServletRequest request, HttpServletResponse response) {
+
+        // 清除cookie
+        CookieUtils.deleteCookie(request, response, "user");
+
+        return IMOOCJSONResult.ok();
+    }
+
 }
