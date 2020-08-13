fcb6e9d0120a621797dc2583dadd27ae06b893e2
zhangyoubao <18301699319@139.com>
2020-08-13 18:08:26 +0800

    收货地址管理

---
+++ foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/AddressService.java
@@ -0,0 +1,20 @@
+package com.imooc.service;
+
+import com.imooc.pojo.UserAddress;
+import com.imooc.pojo.bo.AddressBO;
+
+import java.util.List;
+
+public interface AddressService {
+
+    List<UserAddress> queryAll(String userId);
+
+    void addNewUserAddress(AddressBO addressBO);
+
+    void updateUserAddress(AddressBO addressBO);
+
+    void deleteUserAddress(String userId, String addressId);
+
+    void updateUserAddressToBeDefault(String userId, String addressId);
+
+}
---
+++ foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/impl/AddressServiceImpl.java
@@ -0,0 +1,101 @@
+package com.imooc.service.impl;
+
+import com.imooc.enums.YesOrNo;
+import com.imooc.mapper.UserAddressMapper;
+import com.imooc.pojo.UserAddress;
+import com.imooc.pojo.bo.AddressBO;
+import com.imooc.service.AddressService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    @Resource
+    private UserAddressMapper userAddressMapper;
+
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<UserAddress> queryAll(String userId) {
+        UserAddress ua = new UserAddress();
+        ua.setUserId(userId);
+
+        return userAddressMapper.select(ua);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void addNewUserAddress(AddressBO addressBO) {
+
+        // 1.判断当前用户是否存在地址，如果没有，则新增为默认地址
+        int isDefault = 0;
+        List<UserAddress> addressList = this.queryAll(addressBO.getUserId());
+        if (addressList == null || addressList.size() == 0) {
+            isDefault = 1;
+        }
+
+        // 2.保存地址到数据库
+        UserAddress userAddress = new UserAddress();
+        BeanUtils.copyProperties(addressBO, userAddress);
+
+        userAddress.setId(UUID.randomUUID().toString().replace("-", ""));
+        userAddress.setIsDefault(isDefault);
+        userAddress.setCreatedTime(new Date());
+        userAddress.setUpdatedTime(new Date());
+
+        userAddressMapper.insert(userAddress);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void updateUserAddress(AddressBO addressBO) {
+
+        String addressId = addressBO.getAddressId();
+
+        UserAddress userAddress = new UserAddress();
+        BeanUtils.copyProperties(addressBO, userAddress);
+        userAddress.setId(addressId);
+        userAddress.setUpdatedTime(new Date());
+
+        userAddressMapper.updateByPrimaryKeySelective(userAddress);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void deleteUserAddress(String userId, String addressId) {
+
+        UserAddress userAddress = new UserAddress();
+        userAddress.setId(addressId);
+        userAddress.setUserId(userId);
+        userAddressMapper.delete(userAddress);
+    }
+
+    @Override
+    public void updateUserAddressToBeDefault(String userId, String addressId) {
+
+        // 1.查找默认地址，修改为不默认
+        UserAddress queryAddress = new UserAddress();
+        queryAddress.setUserId(userId);
+        queryAddress.setIsDefault(YesOrNo.YES.type);
+        List<UserAddress> list = userAddressMapper.select(queryAddress);
+        for (UserAddress ua : list) {
+            ua.setIsDefault(YesOrNo.NO.type);
+            userAddressMapper.updateByPrimaryKeySelective(ua);
+        }
+
+        // 2. 根据地址id修改为默认地址
+        UserAddress defaultAddress = new UserAddress();
+        defaultAddress.setId(addressId);
+        defaultAddress.setUserId(userId);
+        defaultAddress.setIsDefault(YesOrNo.YES.type);
+        userAddressMapper.updateByPrimaryKeySelective(defaultAddress);
+    }
+}
